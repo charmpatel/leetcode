ee8791a2b62c3bcb13d772362433563a262acd61
charmpatel <charmi0127@gmail.com>
2024-01-13 17:09:48 -0500

    Commiting 56_Merge_intervals.

---
+++ leetcode/leetcode/LeetCode_Solutions/56_Merge_Intervals.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcode.LeetCode_Solutions
+{
+    public class _56_Merge_Intervals
+    {
+        public _56_Merge_Intervals()
+        {
+            int[][] array = new int[3][] { new int[] { 0, 30 }, new int[] { 5, 10 }, new int[] { 15, 20 } };
+            Console.WriteLine(Merge(array)); // TODO: Figure out how to print int[][] array
+        }
+
+        public int[][] Merge(int[][] intervals)
+        {
+            int n = intervals.Length;
+            if (intervals == null || n == 1)
+            {
+                return intervals;
+            }
+            Array.Sort(intervals, (a, b) => { return a[0] - b[0]; });
+            List<int[]> res = new List<int[]>();
+            int start = intervals[0][0];
+            int end = intervals[0][1];
+            for (int i = 1; i < n; i++)
+            {
+                if (end >= intervals[i][0])
+                {
+                    end = Math.Max(end, intervals[i][1]);
+                }
+                else
+                {
+                    res.Add(new int[] { start, end });
+                    start = intervals[i][0];
+                    end = intervals[i][1];
+                }
+
+            }
+            res.Add(new int[] { start, end });
+            return res.ToArray(); // int[][]
+        }
+                
+    }
+}

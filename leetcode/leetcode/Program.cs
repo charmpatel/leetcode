e31258c8d20653c6fca2fe1ba7704954419c96e1
charmpatel <charmi0127@gmail.com>
2024-01-16 16:41:19 -0500

    56_Merge_intervals jagged array print as a output

--- leetcode/leetcode/Program.cs
+++ leetcode/leetcode/Program.cs
@@ -12,6 +12,7 @@ namespace leetcode
             _56_Merge_Intervals mergeIntervals = new _56_Merge_Intervals();
             //_253_Meeting_Rooms_2 meetingRooms2 = new _253_Meeting_Rooms_2();
             //_252_Meeting_Rooms canAttendMeeting = new _252_Meeting_Rooms();
+            _435_Non_Overlapping_Intervals removeOverlapIntervals = new _435_Non_Overlapping_Intervals();
             Console.WriteLine("Ending Merge Intervals Pattern");
 /*
             Console.WriteLine("Starting Fast and Slow Pointers");

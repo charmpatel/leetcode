e7f2bbcccc6e612abc2bc3e3878735c0b56ab753
charmpatel <charmi0127@gmail.com>
2024-01-16 19:43:15 -0500

    adding code for _2_Add_Two_Numbers

---
+++ leetcode/leetcode/LeetCode_Solutions/Fast-SlowPointers/2_Add_Two_Numbers.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcode.LeetCode_Solutions.Fast_SlowPointers
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class _2_Add_Two_Numbers
+    {
+        public _2_Add_Two_Numbers()
+        {
+            ListNode l1 = new ListNode(2) ;
+            l1.next = new ListNode(4);
+            l1.next.next = new ListNode(3);
+
+            ListNode l2 = new ListNode(5);
+            l2.next = new ListNode(6);
+            l2.next.next = new ListNode(4);
+
+            ListNode result = AddTwoNumbers(l1, l2);
+            PrintResult(result);
+        }
+
+        public void PrintResult(ListNode result)
+        {
+            while(result != null)
+            {
+                Console.WriteLine(result.val);
+                result = result.next;
+            }
+        }
+
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+
+
+            ListNode dummyNode = new ListNode(0);
+            ListNode current = dummyNode;
+            int carry = 0;
+
+            while (l1 != null || l2 != null || carry != 0)
+            {
+                int x = (l1 != null) ? l1.val : 0; //3
+                int y = (l2 != null) ? l2.val : 0; // 4
+                int sum = x + y + carry; //8
+                carry = sum / 10; // 8/10 --> 0 
+                current.next = new ListNode(sum % 10); // 8%10 ==8 
+                current = current.next; // 7
+
+                if (l1 != null)
+                    l1 = l1.next;
+                if (l2 != null)
+                    l2 = l2.next;
+
+            }
+            return dummyNode.next;
+        }
+    }
+}
--- leetcode/leetcode/Program.cs
+++ leetcode/leetcode/Program.cs
@@ -1,4 +1,5 @@
 ﻿using leetcode.LeetCode_Solutions;
+using leetcode.LeetCode_Solutions.Fast_SlowPointers;
 using System;
 
 namespace leetcode
@@ -7,19 +8,19 @@ namespace leetcode
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Starting Merge Intervals Pattern");
+            //Console.WriteLine("Starting Merge Intervals Pattern");
 
-            //_56_Merge_Intervals mergeIntervals = new _56_Merge_Intervals();
-            //_253_Meeting_Rooms_2 meetingRooms2 = new _253_Meeting_Rooms_2();
-            //_252_Meeting_Rooms canAttendMeeting = new _252_Meeting_Rooms();
-            _435_Non_Overlapping_Intervals removeOverlapIntervals = new _435_Non_Overlapping_Intervals();
-            Console.WriteLine("Ending Merge Intervals Pattern");
-/*
+            ////_56_Merge_Intervals mergeIntervals = new _56_Merge_Intervals();
+            ////_253_Meeting_Rooms_2 meetingRooms2 = new _253_Meeting_Rooms_2();
+            ////_252_Meeting_Rooms canAttendMeeting = new _252_Meeting_Rooms();
+            //_435_Non_Overlapping_Intervals removeOverlapIntervals = new _435_Non_Overlapping_Intervals();
+            //Console.WriteLine("Ending Merge Intervals Pattern");
+
             Console.WriteLine("Starting Fast and Slow Pointers");
-
+            _2_Add_Two_Numbers addTwoNums = new _2_Add_Two_Numbers();
             Console.WriteLine("Ending Fast and Slow Pointers");
 
-*/
+
         }
     }
 }

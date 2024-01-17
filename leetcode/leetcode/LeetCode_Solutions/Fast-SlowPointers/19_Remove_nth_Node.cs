40d20a0d789a614a50c0755d4458d4de47c6e7e0
charmpatel <charmi0127@gmail.com>
2024-01-17 14:25:16 -0500

    coding for 19_RemoveNthFromeEnd

---
+++ leetcode/leetcode/LeetCode_Solutions/Fast-SlowPointers/19_Remove_nth_Node.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcode.LeetCode_Solutions.Fast_SlowPointers
+{
+    public class Listnode
+    {
+        int val;
+        Listnode next;
+        public Listnode(int val = 0, Listnode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    public class _19_Remove_nth_Node
+    {
+        public _19_Remove_nth_Node()
+        {
+            ListNode l1 = new ListNode(1);
+            l1.next = new ListNode(2);
+            l1.next.next = new ListNode(3);
+            l1.next.next.next = new ListNode(4);
+            l1.next.next.next.next = new ListNode(5);
+           
+            ListNode head = removeNthFromEnd(l1,4);
+            PrintResult(head);
+        }
+
+        public void PrintResult(ListNode head)
+        {
+            ListNode result = head;
+            while (result != null)
+            {
+                Console.WriteLine(result.val
+                    + " ");
+                result = result.next;
+            }
+            Console.WriteLine();
+        }
+
+        public ListNode removeNthFromEnd(ListNode head, int n)
+        {
+            ListNode slow = head;
+            ListNode fast = head;
+
+            for(int i = 0; i<n; i++)
+            {
+                fast = fast.next;
+            }
+            if(fast == null)
+            {
+                return head.next;
+            }
+            while(fast.next != null)
+            {
+                slow = slow.next;
+                fast = fast.next;
+            }
+            slow.next = slow.next.next;
+            return head;
+        }
+    }
+}
--- leetcode/leetcode/Program.cs
+++ leetcode/leetcode/Program.cs
@@ -10,15 +10,16 @@ namespace leetcode
         static void Main(string[] args)
         {
             Console.WriteLine("Starting Merge Intervals Pattern");
-
-            _56_Merge_Intervals mergeIntervals = new _56_Merge_Intervals();
-            _253_Meeting_Rooms_2 meetingRooms2 = new _253_Meeting_Rooms_2();
-            _252_Meeting_Rooms canAttendMeeting = new _252_Meeting_Rooms();
-            _435_Non_Overlapping_Intervals removeOverlapIntervals = new _435_Non_Overlapping_Intervals();
-            Console.WriteLine("Ending Merge Intervals Pattern");
-
+            /*
+                        _56_Merge_Intervals mergeIntervals = new _56_Merge_Intervals();
+                        _253_Meeting_Rooms_2 meetingRooms2 = new _253_Meeting_Rooms_2();
+                        _252_Meeting_Rooms canAttendMeeting = new _252_Meeting_Rooms();
+                        _435_Non_Overlapping_Intervals removeOverlapIntervals = new _435_Non_Overlapping_Intervals();
+                        Console.WriteLine("Ending Merge Intervals Pattern");
+            */
             Console.WriteLine("Starting Fast and Slow Pointers");
-            _2_Add_Two_Numbers add_Two_Numbers = new _2_Add_Two_Numbers();
+            //_2_Add_Two_Numbers add_Two_Numbers = new _2_Add_Two_Numbers();
+            _19_Remove_nth_Node removeNthFromEnd = new _19_Remove_nth_Node();
             Console.WriteLine("Ending Fast and Slow Pointers");
 
 

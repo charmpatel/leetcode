34b97ff4ae6f045f16970a62ed45f6c306fc2d3c
charmpatel <charmi0127@gmail.com>
2024-01-24 17:11:13 -0500

    Added Input (2D matrix) and printed output.

--- leetcode/leetcode/LeetCode_Solutions/Arrays/54_Spiral_Matrix.cs
+++ leetcode/leetcode/LeetCode_Solutions/Arrays/54_Spiral_Matrix.cs
@@ -8,15 +8,28 @@ namespace leetcode.LeetCode_Solutions.Arrays
     {
         public _54_Spiral_Matrix()
         {
-           
+            int[][] matrix = new int[3][] {
+                new int[] {1,2,3 },
+                new int[] {4,5,6},
+                new int[] {7,8,9 }
+            };
+            
+            var result = spiralMatrix(matrix);
+
+            foreach (var i in result)
+            {
+                Console.Write("{0} ", i);
+            }
+            Console.WriteLine();
+
         }
         public IList<int> spiralMatrix(int[][] matrix)
         {
             IList<int> res = new List<int>();
             int startrow = 0;
             int startcol = 0;
-            int endrow = matrix[0].Length - 1;
-            int endcol = matrix.Length - 1;
+            int endrow = matrix[0].Length - 1; //2
+            int endcol = matrix.Length - 1; //2
             while(startrow <= endrow && startcol <= endcol)
             {
                 for(int i = startrow; i <= endrow; i++)

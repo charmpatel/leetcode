a0292ac2272557c4f5eb758d8cba83e9cc40825f
charmpatel <charmi0127@gmail.com>
2024-01-24 17:16:06 -0500

    Added callers from the constrcutor.

--- leetcode/leetcode/LeetCode_Solutions/Arrays/359_Logger_Rate_Limiter.cs
+++ leetcode/leetcode/LeetCode_Solutions/Arrays/359_Logger_Rate_Limiter.cs
@@ -6,16 +6,26 @@ namespace leetcode.LeetCode_Solutions.Arrays
 {
     public class _359_Logger_Rate_Limiter
     {
+        Dictionary<string, int> dict = new Dictionary<string, int>();
+
         public _359_Logger_Rate_Limiter()
         {
             /*int timestamp = new int { 1, 3, 5, 8, 10 };
             string message1 = new string { "foo", "bar", "here", "foo", "bar" };
             Console.WriteLine(logTime(timestamp, message1));*/
+
+            Console.WriteLine(logTime(1, "foo"));
+            Console.WriteLine(logTime(2, "bar"));
+            Console.WriteLine(logTime(3, "foo"));
+            Console.WriteLine(logTime(8, "bar"));
+            Console.WriteLine(logTime(10, "foo"));
+            Console.WriteLine(logTime(11, "foo"));
+            Console.WriteLine(logTime(17, "foo"));
+            Console.WriteLine(logTime(21, "foo"));
         }
         
         public bool logTime(int timestamp, string message)
         {
-            Dictionary<string, int> dict = new Dictionary<string, int>();
             if (!dict.ContainsKey(message))
             {
                 dict.Add(message, timestamp);

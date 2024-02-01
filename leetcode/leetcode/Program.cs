ea8364173e71545f53458f3f023e0a6f1762ea8c
charmpatel <charmi0127@gmail.com>
2024-01-31 20:05:43 -0500

    Changed  program file

--- leetcode/leetcode/Program.cs
+++ leetcode/leetcode/Program.cs
@@ -8,6 +8,7 @@ using leetcode.LeetCode_Solutions.Arrays_Hashing;
 using leetcode.LeetCode_Solutions.Sliding_Window;
 using System;
 using System.Collections.Generic;
+using leetcode.LeetCode_Solutions.Arrays;
 
 namespace leetcode
 {
@@ -16,21 +17,21 @@ namespace leetcode
         static void Main(string[] args)
         {
             Console.WriteLine("Starting Merge Intervals Pattern");
-            /*
-            _56_Merge_Intervals mergeIntervals = new _56_Merge_Intervals();
+
+           /* _56_Merge_Intervals mergeIntervals = new _56_Merge_Intervals();
             _253_Meeting_Rooms_2 meetingRooms2 = new _253_Meeting_Rooms_2();
             _252_Meeting_Rooms canAttendMeeting = new _252_Meeting_Rooms();
             _435_Non_Overlapping_Intervals removeOverlapIntervals = new _435_Non_Overlapping_Intervals();*/
             Console.WriteLine("Ending Merge Intervals Pattern");
             
             Console.WriteLine("Starting Fast and Slow Pointers");
-            /* _2_Add_Two_Numbers add_Two_Numbers = new _2_Add_Two_Numbers();
-             _19_Remove_nth_Node removeNthFromEnd = new _19_Remove_nth_Node();
-             _83_Remove_Duplicates_From_SortedList deleteDuplicates = new _83_Remove_Duplicates_From_SortedList();
-             _141_Linked_List_Cycle linkedListCycle = new _141_Linked_List_Cycle();
-             _142_LinkedList_cycle_2 detectCycle = new _142_LinkedList_cycle_2();
-             _203_Remove_Linked_List_Elements RemoveElement = new _203_Remove_Linked_List_Elements();
-             _876_Middle_Of_The_LinkedList subLinkedList = new _876_Middle_Of_The_LinkedList();
+            /*_2_Add_Two_Numbers add_Two_Numbers = new _2_Add_Two_Numbers();
+            _19_Remove_nth_Node removeNthFromEnd = new _19_Remove_nth_Node();
+            _83_Remove_Duplicates_From_SortedList deleteDuplicates = new _83_Remove_Duplicates_From_SortedList();
+            _141_Linked_List_Cycle linkedListCycle = new _141_Linked_List_Cycle();
+            _142_LinkedList_cycle_2 detectCycle = new _142_LinkedList_cycle_2();
+            _203_Remove_Linked_List_Elements RemoveElement = new _203_Remove_Linked_List_Elements();
+            _876_Middle_Of_The_LinkedList subLinkedList = new _876_Middle_Of_The_LinkedList();
             _143_Reorder_List reorderList = new _143_Reorder_List();*/
             Console.WriteLine("Ending Fast and Slow Pointers");
 
@@ -45,7 +46,7 @@ namespace leetcode
 
             Console.WriteLine("Starting Arrays");
             /*_54_Spiral_Matrix traversMatrix = new _54_Spiral_Matrix();
-            _1_Two_Sum twoSum = new _1_Two_Sum();            
+            _1_Two_Sum twoSum = new _1_Two_Sum();
             _359_Logger_Rate_Limiter isMessagePrint = new _359_Logger_Rate_Limiter();
             _1763_Longest_Nice_Substring longestSubstring = new _1763_Longest_Nice_Substring();
             _268_Missing_Number missingNumber = new _268_Missing_Number();
@@ -57,21 +58,21 @@ namespace leetcode
             _266_Palindrome_Permutation permutePalindrome = new _266_Palindrome_Permutation();
             _1822_Sign_Of_the_Product_Of_An_Array signOfArray = new _1822_Sign_Of_the_Product_Of_An_Array();
             _1304_Find_N_Unique_Integers_Sum_UpTo_Zero uniqueIntSumZero = new _1304_Find_N_Unique_Integers_Sum_UpTo_Zero();*/
-            
+
             Console.WriteLine("Ending Arrays");
 
             Console.WriteLine("Starting Two_Pointers");
             /*_15_3_Sum ThreeSum = new _15_3_Sum();
             _125_Valid_Palindrome isPalindrome = new _125_Valid_Palindrome();
-           _283_Move_Zeroes moveZeroesAtEnd = new _283_Move_Zeroes();
-           _11_Container_With_Most_Water maxAmountWater = new _11_Container_With_Most_Water();
-           _234_Palindrome_Linked_List palindrome_Linked_List = new _234_Palindrome_Linked_List();
-           _148_Sort_List sortList = new _148_Sort_List();
+            _283_Move_Zeroes moveZeroesAtEnd = new _283_Move_Zeroes();
+            _11_Container_With_Most_Water maxAmountWater = new _11_Container_With_Most_Water();
+            _234_Palindrome_Linked_List palindrome_Linked_List = new _234_Palindrome_Linked_List();
+            _148_Sort_List sortList = new _148_Sort_List();
             _1_Two_Sum_Sorted_Array twoSumSortedArray = new _1_Two_Sum_Sorted_Array();*/
             Console.WriteLine("Ending Two Pointers");
 
             Console.WriteLine("Starting Sliding Windows");
-            _3_Longest_Substring_Without_Repeating_characters longestSubstringUniquechar = new _3_Longest_Substring_Without_Repeating_characters();
+            /*_3_Longest_Substring_Without_Repeating_characters longestSubstringUniquechar = new _3_Longest_Substring_Without_Repeating_characters();
             _155_Min_Stack getMinStackVal = new _155_Min_Stack();
             getMinStackVal.Push(-2);
             getMinStackVal.Push(0);
@@ -82,7 +83,7 @@ namespace leetcode
             Console.WriteLine("Get Minimum Val of Stack {0} ", getMinStackVal.GetMin()); // return -2
 
             _150_Evaluate_Reverse_Polish_Notation reversearithmeticExpression = new _150_Evaluate_Reverse_Polish_Notation();
-            _20_Valid_Parentheses validParenth = new _20_Valid_Parentheses();
+            _20_Valid_Parentheses validParenth = new _20_Valid_Parentheses();*/
             Console.WriteLine("Ending Sliding Windows");
           
         }

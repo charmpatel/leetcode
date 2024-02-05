ab692a1ff6cec5834fc8f57033fde4bb82038041
charmpatel <charmi0127@gmail.com>
2024-02-05 09:28:09 -0500

    Final update for 1_Two_Sum

--- leetcode/leetcode/LeetCode_Solutions/Arrays_Hashing/1_Two_Sum.cs
+++ leetcode/leetcode/LeetCode_Solutions/Arrays_Hashing/1_Two_Sum.cs
@@ -1,10 +1,21 @@
-﻿/****
- * Question# 1 : Two Sum
- * 
- * 
- * 
- * 
- *///
+﻿/* 1 - Two Sum.
+   Topic - Array, Hash Table.
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+Example 1:
+Input: nums = [2, 7, 11, 15], target = 9
+Output:[0,1]
+Explanation: Because nums[0] +nums[1] == 9, we return [0, 1].
+Example 2:
+Input: nums = [3, 2, 4], target = 6
+Output:[1,2]
+Example 3:
+Input: nums = [3, 3], target = 6
+Output:[0,1]
+*/
+
+
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -20,32 +31,27 @@ namespace leetcode.LeetCode_Solutions.Arrays
             int[] res = twoSum(nums, target);
             for(int i = 0; i<res.Length; i++)
             {
-                Console.WriteLine(res[i]);
+                Console.WriteLine("Two Sum {0} ", res[i]);
             }
             
         }
-       
-        public int[] twoSum(int[] nums ,int target)
+         public int[] twoSum(int[] nums, int target)
         {
             Dictionary<int, int> dict = new Dictionary<int, int>();
-
-
-            for (int i = 0; i <= nums.Length; i++)
+            for(int i = 0; i <= nums.Length; i++)
             {
-
-                int x = target - nums[i]; // x = 9 -2 = 2
-
+                int x = target - nums[i];
                 if (dict.ContainsKey(x))
                 {
                     return new int[] { dict[x], i };
                 }
-
                 if (!dict.ContainsKey(nums[i]))
                 {
-                    dict[nums[i]] = i; // dict <2,0>
+                    dict[nums[i]] = i;
                 }
             }
             return new int[0];
         }
+
     }
 }

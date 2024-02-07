fadd799611160259c9adb2983c655a8b833dd76d
charmpatel <charmi0127@gmail.com>
2024-02-07 12:54:36 -0500

    updated 359_Logger rate limiter

--- leetcode/leetcode/LeetCode_Solutions/Arrays_Hashing/359_Logger_Rate_Limiter.cs
+++ leetcode/leetcode/LeetCode_Solutions/Arrays_Hashing/359_Logger_Rate_Limiter.cs
@@ -1,5 +1,5 @@
 ﻿/* 359 - Logger Rate Limiter
-   Topic - 
+   Topic - Hash Table, Design, Data Stream.
 Design a logger system that receives a stream of messages along with their timestamps. Each unique message should only be printed at most every 10 seconds (i.e. a message printed at timestamp t will prevent other identical messages from being printed until timestamp t + 10).
 All messages will come in chronological order. Several messages may arrive at the same timestamp.
 Implement the Logger class:

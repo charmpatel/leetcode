3626f61e6b0a0f6aea6e30e214ecb708e6252544
charmpatel <charmi0127@gmail.com>
2024-02-12 09:38:54 -0500

    Updated question in _143_Reorder_List

--- leetcode/leetcode/LeetCode_Solutions/Fast-SlowPointers/143_Reorder_List.cs
+++ leetcode/leetcode/LeetCode_Solutions/Fast-SlowPointers/143_Reorder_List.cs
@@ -1,4 +1,20 @@
-﻿using leetcode.LeetCode_Solutions.Utility;
+﻿/* 143 - Reorder List.
+ * Topic - 
+You are given the head of a singly linked-list.The list can be represented as:
+L0 → L1 → … → Ln - 1 → Ln
+Reorder the list to be on the following form:
+L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+You may not modify the values in the list's nodes. Only nodes themselves may be changed.
+Example 1:
+Input: head = [1,2,3,4]
+        Output: [1,4,2,3]
+Example 2:
+Input: head = [1,2,3,4,5]
+Output: [1,5,2,4,3]
+*/
+
+
+using leetcode.LeetCode_Solutions.Utility;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -25,17 +41,7 @@ namespace leetcode.LeetCode_Solutions.Two_Pointers
                 result = result.next;
             }
         }
-        /*You are given the head of a singly linked-list.The list can be represented as:
-        L0 → L1 → … → Ln - 1 → Ln
-        Reorder the list to be on the following form:
-        L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
-        You may not modify the values in the list's nodes. Only nodes themselves may be changed.
-        Example 1:
-        Input: head = [1,2,3,4]
-                Output: [1,4,2,3]
-        Example 2:
-        Input: head = [1,2,3,4,5]
-        Output: [1,5,2,4,3]*/
+        
 
         public void recorderList(ListNode head)
         {

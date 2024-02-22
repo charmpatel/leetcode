a9a05ae99eabdb3f77c430dd81f61d46ed4ec47a
charmpatel <charmi0127@gmail.com>
2024-02-22 10:10:19 -0500

    updated for 148-Sort List

--- leetcode/leetcode/LeetCode_Solutions/Two_Pointers/148_Sort_List.cs
+++ leetcode/leetcode/LeetCode_Solutions/Two_Pointers/148_Sort_List.cs
@@ -1,4 +1,18 @@
-﻿using leetcode.LeetCode_Solutions.Utility;
+﻿/* 148 - Sort List
+Topic - Linked List,Two Pointer, Divide and Conquer, Sorting, Merge Sort.
+Given the head of a linked list, return the list after sorting it in ascending order.
+Example 1:
+Input: head = [4, 2, 1, 3]
+Output: [1,2,3,4]
+Example 2:
+Input: head = [-1, 5, 3, 4, 0]
+Output: [-1,0,3,4,5]
+Example 3:
+Input: head = []
+Output: []
+*/
+
+using leetcode.LeetCode_Solutions.Utility;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -10,10 +24,10 @@ namespace leetcode.LeetCode_Solutions.Two_Pointers
         public _148_Sort_List()
         {
             ListNode head = new ListNode(4);
-            head.next = new ListNode(2);
+            head.next = new ListNode(5);
             head.next.next = new ListNode(1);
             head.next.next.next = new ListNode(3);
-            head.next.next.next.next = new ListNode(5);
+            head.next.next.next.next = new ListNode(2);
             ListNode result = sortList(head);
             PrintResult(result);
 
@@ -26,47 +40,25 @@ namespace leetcode.LeetCode_Solutions.Two_Pointers
                 result = result.next;
             }
         }
-    
-        /*Given the head of a linked list, return the list after sorting it in ascending order.
-        Example 1:
-        Input: head = [4, 2, 1, 3]
-        Output: [1,2,3,4]
-        Example 2:
-        Input: head = [-1, 5, 3, 4, 0]
-        Output: [-1,0,3,4,5]
-        Example 3:
-        Input: head = []
-        Output: []*/
         public ListNode sortList(ListNode head)
         {
             if (head == null)
                 return null;
-
-            var sorted = toSortedList(head);
-            return buildList(sorted);
-        }
-        private List<int> toSortedList(ListNode head)
-        {
-            var res = new List<int>();
+            var list = new List<int>();
+            var curr = new ListNode();
+            var p = curr;
             while (head != null)
             {
-                res.Add(head.val);
+                list.Add(head.val);
                 head = head.next;
-            }
-            res.Sort();
-            return res;
-        }
-
-        private ListNode buildList(List<int> list)
-        {
-            var root = new ListNode(list[0]);
-            var curr = root;
-            for (int i = 1; i < list.Count; i++)
+            }  
+            list.Sort();
+            for (int i = 0; i < list.Count; i++)
             {
-                curr.next = new ListNode(list[i]);
-                curr = curr.next;
+                p.next = new ListNode(list[i]);
+                p = p.next;
             }
-            return root;
+            return curr.next;
         }
     }
 }

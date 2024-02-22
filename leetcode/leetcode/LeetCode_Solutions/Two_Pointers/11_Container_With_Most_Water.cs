f84d7219a647e8cc03cbf8407156f3f8b9e80ee4
charmpatel <charmi0127@gmail.com>
2024-02-22 10:31:06 -0500

    Updated for _11_Container_With_Most_Water

--- leetcode/leetcode/LeetCode_Solutions/Two_Pointers/11_Container_With_Most_Water.cs
+++ leetcode/leetcode/LeetCode_Solutions/Two_Pointers/11_Container_With_Most_Water.cs
@@ -1,4 +1,19 @@
-﻿using System;
+﻿/* 11 - Container With Most Water
+ Topic - Two Pointers,Array, Greedy.
+ You are given an integer array height of length n.There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and(i, height[i]).
+Find two lines that together with the x-axis form a container, such that the container contains the most water.
+Return the maximum amount of water a container can store.
+Notice that you may not slant the container.
+Example 1:
+Input: height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
+Output: 49
+Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+Example 2:
+Input: height = [1, 1]
+Output: 1
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -11,17 +26,7 @@ namespace leetcode.LeetCode_Solutions.Two_Pointers
             int[] height = new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
             Console.WriteLine(maxWater(height));
         }
-       /* You are given an integer array height of length n.There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and(i, height[i]).
-        Find two lines that together with the x-axis form a container, such that the container contains the most water.
-        Return the maximum amount of water a container can store.
-        Notice that you may not slant the container.
-        Example 1:
-        Input: height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
-        Output: 49
-        Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
-        Example 2:
-        Input: height = [1, 1]
-        Output: 1*/
+      
 
         public int maxWater(int[] height)
         {

6847bcd492b29e95e8341ffc4f4f306b4b07867e
charmpatel <charmi0127@gmail.com>
2024-02-22 13:31:01 -0500

    Update for _680_Valid_Palindrome_2

--- leetcode/leetcode/LeetCode_Solutions/Two_Pointers/680_Valid_Palindrome_2.cs
+++ leetcode/leetcode/LeetCode_Solutions/Two_Pointers/680_Valid_Palindrome_2.cs
@@ -1,4 +1,19 @@
-﻿using System;
+﻿/* 680 - Valid Planidrome 2.
+Topic - Two Pointers, String , Greedy.
+Given a string s, return true if the s can be palindrome after deleting at most one character from it.
+Example 1:
+Input: s = "aba"
+Output: true
+Example 2:
+Input: s = "abca"
+Output: true
+Explanation: You could delete the character 'c'.
+Example 3:
+Input: s = "abc"
+Output: false
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -11,22 +26,11 @@ namespace leetcode.LeetCode_Solutions.Two_Pointers
             string s = "abba";
             string s1 = "abca";
             string s2 = "abcdef";
-            Console.WriteLine(validPalindrome2(s));
-            Console.WriteLine(validPalindrome2(s1));
-            Console.WriteLine(validPalindrome2(s2));
+            Console.WriteLine("Valid Planidrome 2 {0}", validPalindrome2(s));
+            Console.WriteLine("Valid Planidrome 2 {0}", validPalindrome2(s1));
+            Console.WriteLine("Valid Planidrome 2 {0}", validPalindrome2(s2));
         }
-        /*Given a string s, return true if the s can be palindrome after deleting at most one character from it.
-        Example 1:
-        Input: s = "aba"
-        Output: true
-        Example 2:
-        Input: s = "abca"
-        Output: true
-        Explanation: You could delete the character 'c'.
-        Example 3:
-        Input: s = "abc"
-        Output: false*/
-
+        
         public bool validPalindrome2(string s)
          {
              int left = 0;
